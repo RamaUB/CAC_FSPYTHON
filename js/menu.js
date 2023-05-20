691f2426b483c0dd9e60ab7344ba487742f50bb0
CCIAFA <129988229+CCIAFA@users.noreply.github.com>
2023-05-19 21:40:15 -0300

    Update menu.js y simplificacion de funcion armarCajas

--- js/menu.js
+++ js/menu.js
@@ -1,52 +1,16 @@
 function armarCajas(pizza){
-    // Se crean los elementos que contiene la caja
-    let li = document.createElement("li");
-    let div = document.createElement("div");
-    let div2 = document.createElement("div");
-    let div3 = document.createElement("div");
-    let imagen = document.createElement("img");
-
-    //Se carga los datos contenido en la etiqueta img dentro del div2
-    imagen.src = pizza.imagen;
-    imagen.classList = "imagenComida";
-    div2.appendChild(imagen);
-
-    // Se agrega el div2 al div principal dentro del item
-    div.appendChild(div2);
-
-    // Se cargan los datos de nombre, descripcion y precio dentro del div3
-    // que son contenidos dentro del div principal
-    let nombre = document.createElement("h2");
-    nombre.textContent = pizza.nombre;
-    div3.appendChild(nombre);
-    let descripcion = document.createElement("p");
-    descripcion.textContent = pizza.descripcion;
-    div3.appendChild(descripcion);
-    let precio = document.createElement("p");
-    precio.textContent = "$" + pizza.precio;
-    precio.classList = "producto-precio";
-    div3.appendChild(precio);
-
-    // Se agrega el div3 al div principal del item
-    div.appendChild(div3);
-
-    // Se carga el div principal al item li
-    li.appendChild(div);
-
-    return li;
-}
-
-function armarCajas2(pizza){
     li = document.createElement("li");
-    let codigoCaja = '<div><div><img class="imagenComida" src="'+
-    pizza.imagen+
-    '" alt=""></div><div><h2>'+
-    pizza.nombre+'</h2><p>'
-    +pizza.descripcion+
-    '</p><p>'
-    +pizza.precio+
-    '</p></div></div>';
-    li.innerHTML = codigoCaja;
+    li.innerHTML = `
+        <div>
+            <div>
+                <img class="imagenComida" src="${pizza.imagen}" alt="">
+            </div>
+            <div>
+                <h2>${pizza.nombre}</h2>
+                <p>${pizza.descripcion}</p>
+                <p>${pizza.precio}</p>
+            </div>
+        </div>`
     return li;
 }
     
